5909413d5ef5b75be916be7fe64aaac5dec2da30
Evan Hobbs <evanhobbs@gmail.com>
2018-08-20 11:12:33 -0700

    Node api

---
+++ server.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const _ = require('lodash');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Rough code to let us store notes in memory with out any DB complexity
+const notes = {
+    1: {
+        'id': 1,
+        'title': 'Test Note',
+        'body': 'This is a test note object that will be available by default',
+        'created_at': Date.now(),
+        'created_by': 'admin',
+        'edit_history': [{
+            'edited_at': Date.now(),
+            'edited_by': 'A User',
+        }],
+    },
+}
+
+
+const getNextId = () => {
+  return _.parseInt(
+    _.maxBy(Object.keys(notes), _.parseInt)
+  ) + 1;
+}
+
+const addNote = ({ title, body, created_by }) => {
+  const id = getNextId();
+  notes[id] = {
+    id: id,
+    title: title,
+    body: body,
+    created_by: created_by,
+    created_at: Date.now(),
+    edit_history: [],
+  }
+  return notes[id]
+}
+
+const getNotes = () => notes;
+
+const getNote = (id) => {
+  const note = getNotes()[id];
+  return note ? note : null;
+}
+
+const updateNote = (id, update) => {
+  const note = getNote(id);
+  _.assign(
+    note,
+    _.pick(update, ['title', 'body', 'edited_by'])
+  );
+  note.edit_history.push({
+    edited_by: update.edited_by,
+    edited_at: Date.now(),
+  });
+  return note;
+}
+
+const removeNote = (id) => {
+  if (getNote(id)) {
+    delete getNotes()[req.params.id]
+    return true;
+  }
+  return false;
+
+}
+
+app.post('/notes', (req, res) => {
+  const note = addNote(req.body);
+  res.json(note)
+})
+
+app.get('/notes', (req, res) => {
+  res.json(_.map(getNotes()))
+})
+
+app.get('/notes/:id', (req, res) => {
+  const note = getNote(req.params.id);
+  res.json(note)
+})
+
+app.post('/notes/:id', (req, res) => {
+  const note = updateNote(req.params.id, req.body);
+  res.json(note)
+})
+
+app.delete('/notes/:id', (req, res) => {
+  const result = removeNote(req.params.id);
+  return result;
+})
+
+app.listen(5000, () => console.log('Server running on port 5000'))
